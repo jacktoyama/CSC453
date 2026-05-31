d719a43b772be541a3f0f7e091a0176c6176250c
jacktoyama <jtoyama@calpoly.edu>
2026-05-31 14:05:32 -0700

    starting on libDisk

--- Project4/libDisk.c
+++ Project4/libDisk.c
@@ -3,17 +3,27 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <math.h>
 
 #include "libDisk.h"
 
 int openDisk(char *filename, int nBytes) {
+
+	int diskSize = 0;
+
     FILE *disk = fopen(filename, "r");
 	if (disk == NULL) {
 		fprintf(stderr, "ERROR: fopen().\n");
 		exit(1);
 	}
 
+	if (nBytes > BLOCKSIZE) {
+		return EXIT_FAILURE;
+	}
+
     if (nBytes % BLOCKSIZE != 0) {
-        
+        diskSize = nBytes - (nBytes % BLOCKSIZE);
     }
+
+	
 }
