157d6c60b8696f3e6b76d7010ab74816e88c003e
WhoEmi? <emi-chan@WhoEmis-MBP.lan>
2026-06-03 00:28:22 -0700

    libDisk completed (not tested)

--- Project4/libDisk.c
+++ Project4/libDisk.c
@@ -8,22 +8,84 @@
 #include "libDisk.h"
 
 int openDisk(char *filename, int nBytes) {
-
 	int diskSize = 0;
+    int disk;
 
-    FILE *disk = fopen(filename, "r");
-	if (disk == NULL) {
-		fprintf(stderr, "ERROR: fopen().\n");
-		exit(1);
+	// Content is not overwritten
+	if (nBytes == 0) {
+		// Open an existing disk
+		disk = open(filename, O_RDWR); // O_RDWR for writeDisk later
+		if (disk < 0) {
+			fprintf(stderr, "ERROR: open().\n");
+			return -1;
+		}
+		return disk;
 	}
 
-	if (nBytes > BLOCKSIZE) {
-		return EXIT_FAILURE;
+	// If nBytes less than BLOCKSIZE, return failure
+	if (nBytes < BLOCKSIZE) {
+		return -1;
 	}
-
-    if (nBytes % BLOCKSIZE != 0) {
-        diskSize = nBytes - (nBytes % BLOCKSIZE);
-    }
-
 	
+	// nBytes is not a multiple of BLOCKSIZE check
+    if (nBytes % BLOCKSIZE != 0) {
+        diskSize = nBytes - (nBytes % BLOCKSIZE); // Closest multiple of BLOCKSIZE that is lower than nByte
+		disk = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666); // O_TRUNC erases data in a file
+		if (disk < 0) {
+			fprintf(stderr, "ERROR: open().\n");
+			return -1;	
+		}
+		if (ftruncate(disk, diskSize) < 0) { // Truncate the disk to diskSize, which is 256 aligned
+			fprintf(stderr, "ERROR: ftruncate().\n");
+			close(disk);
+			return -1;
+		}
+		return disk; // file descriptor 
+	}
+}
+
+// Close an existing disk
+int closeDisk(int disk) {
+	if (close(disk) != 0) {
+		return -1;
+	}
+	return 0;
+}
+
+// Read a block of bytes into a local buffer
+int readBlock(int disk, int bNum, void *block) {
+	// Read this block of code
+	if (lseek(disk, bNum * BLOCKSIZE, SEEK_SET) < 0) {
+		fprintf(stderr, "ERROR: lseek().\n");
+		return -1;
+	}
+
+	// Read into local buffer 
+	ssize_t bytesRead = read(disk, block, BLOCKSIZE);
+	if (bytesRead > 0) {
+		block[bytesRead] = '\0'; // null-terminate
+	} else {
+		fprintf(stderr, "ERROR: read().\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+// Write block
+int writeBlock(int disk, int bNum, void *block) {
+	// Go to this block of data
+	if (lseek(disk, bNum * BLOCKSIZE, SEEK_SET) < 0) {
+		fprintf(stderr, "ERROR: lseek().\n");
+		return -1;
+	}
+
+	// Write to this part of the disk
+	ssize_t bytesWrite = write(disk, block, BLOCKSIZE);
+	if (bytesWrite == -1) {
+		fprintf(stderr, "ERROR: write().\n");
+		return -1;
+	}
+
+	return 0;
 }
--- Project4/libDisk.h
+++ Project4/libDisk.h
@@ -1,9 +1,17 @@
+#ifndef LIB_DISK_H
+#define LIB_DISK_H
+
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 #define BLOCKSIZE 256
 
+// Disk Emulator Functions 
 int openDisk(char *filename, int nBytes);
-
 int closeDisk(int disk);
-
 int readBlock(int disk, int bNum, void *block);
-
 int writeBlock(int disk, int bNum, void *block);
+
+
+#endif
