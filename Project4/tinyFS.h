b4d1985d94bbc5f79b4a5b431f19ed2630f3956f
jacktoyama <jtoyama@calpoly.edu>
2026-06-08 21:04:22 -0700

    20 points down, 20 to go

--- Project4/tinyFS.h
+++ Project4/tinyFS.h
@@ -18,15 +18,36 @@ typedef int fileDescriptor;
 #define MAX_OPEN_FILE 10
 #define INODE_FIRST_EXTENT 16
 
-// Functions to use
+/* Inode timestamp byte offsets */
+#define INODE_CREATE_TIME  17   /* bytes 17-20: creation timestamp (uint32_t, little-endian) */
+#define INODE_MODIFY_TIME  21   /* bytes 21-24: last modification timestamp */
+#define INODE_ACCESS_TIME  25   /* bytes 25-28: last access timestamp */
+
+/* FileInfo struct returned by tfs_readFileInfo() */
+typedef struct {
+    char     name[9];        /* null-terminated filename (up to 8 chars) */
+    int      fileSize;       /* file size in bytes */
+    time_t   createTime;     /* creation time */
+    time_t   modifyTime;     /* last modification time */
+    time_t   accessTime;     /* last access time */
+} FileInfo;
+
+/* Core functions */
 int tfs_mkfs(char *filename, int nBytes);
 int tfs_mount(char *diskname);
 int tfs_unmount(void);
 fileDescriptor tfs_openFile(char *name);
 int tfs_closeFile(fileDescriptor FD);
-int tfs_writeFile(fileDescriptor FD,char *buffer, int size);
+int tfs_writeFile(fileDescriptor FD, char *buffer, int size);
 int tfs_deleteFile(fileDescriptor FD);
 int tfs_readByte(fileDescriptor FD, char *buffer);
 int tfs_seek(fileDescriptor FD, int offset);
+
+/* Extra feature (b): Directory listing and file renaming */
+int tfs_readdir(void);
+int tfs_rename(fileDescriptor FD, char *newName);
+
+/* Extra feature (e): Timestamps */
+int tfs_readFileInfo(fileDescriptor FD, FileInfo *info);
 
 #endif
